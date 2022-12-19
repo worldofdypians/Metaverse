5d765ca5edc60e9e4a0026032a4386960f1f4fc2
unknown <aldialinj0@gmail.com>
2022-12-19 10:52:27 +0100

    Added background and font poppins

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import Home from "./screens/Home/Home";
 import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './app.scss'
 
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+     <div className="container-fluid p-0 main-wrapper">
+     <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
+     </div>
     </BrowserRouter>
   );
 }
