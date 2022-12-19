323b17498ec5e898ebdafb65b37ea94c3e36acdf
LorenaL579 <lorena.lici579@gmail.com>
2022-12-19 11:12:57 +0100

    added bootstrap

---
+++ src/screens/Home/GameMarketplace/Marketplace.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MarketPlace = () => {
+  return (
+    <div className="d-flex justify-content-center pt-2 text-white">
+      marketplace
+    </div>
+  );
+};
+
+export default MarketPlace;
