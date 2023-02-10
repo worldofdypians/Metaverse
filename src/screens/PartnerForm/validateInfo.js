dc369c7fa0bcc3039ba7f5e06c1db930ac72c92b
AldiAlinj <aldialinj0@gmail.com>
2023-02-10 15:46:00 +0100

    Started partner form

---
+++ src/screens/PartnerForm/validateInfo.js
@@ -0,0 +1,22 @@
+export default function validateBetaInfo(values, interests) {
+    let errors = {};
+  
+    if (!values.email) {
+      errors.email = "Email Required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = "Email Address is Invalid";
+    }
+    if (!values.name.trim()) {
+      errors.name = "Name is Required";
+    } 
+   
+    if (!values.description.trim()) {
+      errors.description = "Description is Required";
+    } 
+    if (!interests.trim()) {
+      errors.interests = "At least one field of interest is Required";
+    } 
+
+    return errors;
+  }
+  
