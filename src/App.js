417a33c0831a1e815352e7d8a2a50054fb3ee29a
LorenaL579 <lorena.lici579@gmail.com>
2023-02-20 09:19:08 +0100

    removed popup

--- src/App.js
+++ src/App.js
@@ -151,9 +151,9 @@ function App() {
   return (
     <BrowserRouter>
       <div className="container-fluid p-0 main-wrapper position-relative">
-        {!window.location.href.includes('/land') &&
+        {/* {!window.location.href.includes('/land') &&
         <LandPopup />
-        }
+        } */}
         <Header handleSignUp={handleSignUp} />
         <MobileNavbar handleSignUp={handleSignUp} />
         <Routes>
