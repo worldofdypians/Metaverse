6d3f8a3ed06ca5bef65aad87a5680406a4ac45d8
Aldi <aldialinj0@gmail.com>
2023-04-19 19:00:41 +0200

    fixed hero text for firefox

--- src/screens/Timepiece/TimepieceHero.js
+++ src/screens/Timepiece/TimepieceHero.js
@@ -6,7 +6,7 @@ const TimepieceHero = () => {
     <div className="full-screen-main-hero d-flex align-items-center">
         <div className="container-fluid px-3 px-lg-5 position-relative">
           <div className="row justify-content-center pb-5">
-            <div className="col-12 col-lg-8 col-xxl-7 d-flex flex-column align-items-center">
+            <div className="col-12 col-lg-8  d-flex flex-column align-items-center">
               <h6 className="timepiece-title font-organetto d-flex flex-column flex-lg-row gap-2">
                 Caws Timepiece
                 <span
