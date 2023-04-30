a8c62ef9fe7aec14c94409ddfcc76a3896322049
Aldi <aldialinj0@gmail.com>
2023-04-30 20:11:12 +0200

    added images and content to utilities

--- src/screens/Home/Utilities/Utilities.js
+++ src/screens/Home/Utilities/Utilities.js
@@ -1,27 +1,56 @@
 import React from "react";
-import './_utilities.scss'
+import "./_utilities.scss";
 
 const Utilities = () => {
   const utilities = [
     {
-      title: "DYP Token",
+      title: "Live on Mainnet",
       image: "dyp.svg",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere possimus qui dolorum similique praesentium fugiat magnam obcaecati deleniti accusantium, voluptatibus iure cupiditate voluptatem numquam, sint provident, impedit repudiandae deserunt esse.",
+      desc: `
+      <p class="utility-desc">
+      Launched on December 29, 2022, World of Dypians it is currently live on both the Ethereum and BNB Chain mainnets. By being on the mainnet, users can participate in activities and integrations in real-time and explore the benefits of blockchain technology.
+      </p>`,
+      class: "mainnet-card",
     },
     {
-      title: "iDYP Token",
+      title: "Stats",
       image: "idyp.svg",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere possimus qui dolorum similique praesentium fugiat magnam obcaecati deleniti accusantium, voluptatibus iure cupiditate voluptatem numquam, sint provident, impedit repudiandae deserunt esse.",
+      desc: `
+      <ul class="utility desc">
+      <li style="text-align: left" class="utility-desc">Beta demo game with 3,000 beta testers currently enjoying it.</li>
+      <li style="text-align: left" class="utility-desc">7,000 applicants are waiting for beta access.</li>
+      <li style="text-align: left" class="utility-desc">1,300 bundles sold of Dragon Ruins Event.</li>
+      <li style="text-align: left" class="utility-desc">4 new events are currently available for users to participate in.</li>
+      </ul>
+      `,
+      class: "stats-card",
     },
     {
-      title: "CAWS",
+      title: "NFT Integrations",
       image: "caws.png",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere possimus qui dolorum similique praesentium fugiat magnam obcaecati deleniti accusantium, voluptatibus iure cupiditate voluptatem numquam, sint provident, impedit repudiandae deserunt esse.",
+      desc: `
+      <p class="utility-desc">
+      World of Dypians integrates two NFT collections, CAWS and Genesis Land. CAWS are 3D creatures that act as AI-powered companions, while Genesis Land offers users their own plot of virtual land to develop. These collections enhance the user experience and provide new opportunities for self-expression within the metaverse.      
+      </p>`,
+      class: "integrations-card",
     },
     {
-      title: "Genesis Land",
+      title: "Utilities",
       image: "genesis.png",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere possimus qui dolorum similique praesentium fugiat magnam obcaecati deleniti accusantium, voluptatibus iure cupiditate voluptatem numquam, sint provident, impedit repudiandae deserunt esse.",
+      desc: `
+      <p class="utility-desc">
+      The DYP token is the main mode of payment within the World of Dypians metaverse. It is used for in-game purchases, unlocking features and upgrades, and accessing other content. By integrating a decentralized utility token, the game enhances the player experience and drives growth of the World of Dypians ecosystem.      
+      </p>`,
+      class: "coins-card",
+    },
+    {
+      title: "Premium Events",
+      image: "genesis.png",
+      desc:`
+      <p class="utility-desc">
+      A wide range of exclusive features and benefits to its users, and one way to gain access to these is by participating in live events. There are four events such as Golden Pass, Puzzle Madness, Dragon Ruins, and Critical Hit. Users can unlock new opportunities from custom integrations and activities on-chain, as well as earn rewards and recognition within the World of Dypians community.      
+      </p>`,
+      class: "events-card",
     },
   ];
 
@@ -32,20 +61,39 @@ const Utilities = () => {
           Product <mark className="font-organetto explore-tag">Utilities</mark>
         </h2>
       </div>
-      <div className="utilities-grid">
-        {utilities.map((item, index) => (
-            <div className="utility-card flex-column d-flex align-items-center justify-content-center p-4">
-                <div className="utility-first d-flex align-items-center justify-content-center gap-2 flex-column">
-                    <img src={require(`./assets/${item.image}`)} style={{width: '50px', height: '50px'}} alt="" />
-                    <h6 className="utility-title">{item.title}</h6>
-                </div>
-                <div className="utility-desc-div">
-                    <p className="utility-desc">
-                        {item.desc}
-                    </p>
-                </div>
+      <div className="row d-flex align-items-center justify-content-between gap-5 mx-1 mx-lg-0 gap-lg-0">
+        {utilities.slice(0, 3).map((item, index) => (
+          <div key={index} className={`${item.class} col-12 col-lg-4 col-xxl-3 utility-card flex-column d-flex align-items-center justify-content-center p-4`}>
+            <div className="utility-first d-flex align-items-center justify-content-center gap-2 flex-column">
+              {/* <img
+                src={require(`./assets/${item.image}`)}
+                style={{ width: "50px", height: "50px" }}
+                alt=""
+              /> */}
+              <h6 className="utility-title">{item.title}</h6>
             </div>
-        ) )}
+            <div className="utility-desc-div" dangerouslySetInnerHTML={{__html: item.desc}}>
+              {/* <p className="utility-desc">{item.desc}</p> */}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="row d-flex align-items-center justify-content-evenly mx-1 mx-lg-0 gap-5 gap-lg-0 mt-5">
+        {utilities.slice(3, 5).map((item, index) => (
+          <div key={index} className={`${item.class} col-12 col-lg-4 col-xxl-3 utility-card flex-column d-flex align-items-center justify-content-center p-4`}>
+            <div className="utility-first d-flex align-items-center justify-content-center gap-2 flex-column">
+              {/* <img
+                src={require(`./assets/${item.image}`)}
+                style={{ width: "50px", height: "50px" }}
+                alt=""
+              /> */}
+              <h6 className="utility-title">{item.title}</h6>
+            </div>
+            <div className="utility-desc-div" dangerouslySetInnerHTML={{__html: item.desc}}>
+              {/* <p className="utility-desc">{item.desc}</p> */}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
