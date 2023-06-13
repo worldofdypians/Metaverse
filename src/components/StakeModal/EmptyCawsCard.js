b21d2df2a1def78bb898f06146ee0f2cfb3e3b2e
AldiAlinj <aldialinj0@gmail.com>
2023-06-13 16:40:10 +0200

    started on filters and stake modal

---
+++ src/components/StakeModal/EmptyCawsCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import emptyCaws from './assets/emptyCaws.svg'
+
+
+const EmptyCawsCard = () => {
+  return (
+    <div className="nft-modal-card">
+    <div className="d-flex flex-column">
+        <div className="empty-img-wrapper d-flex align-items-center justify-content-center p-4 w-100">
+            <img src={emptyCaws} className='empty-image' alt="" />
+        </div>
+        <div className="empty-card-info p-2 w-100">
+            <span className="info-text">Get your CAWS NFT from the WoD Game Shop</span>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default EmptyCawsCard
