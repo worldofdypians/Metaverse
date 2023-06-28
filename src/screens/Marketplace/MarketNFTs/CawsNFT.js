36e690f2c49b334cd7f04ba82e45000de18e1300
AldiAlinj <aldialinj0@gmail.com>
2023-06-28 12:04:32 +0200

    started filters for collections

--- src/screens/Marketplace/MarketNFTs/CawsNFT.js
+++ src/screens/Marketplace/MarketNFTs/CawsNFT.js
@@ -8,6 +8,12 @@ import searchIcon from "../assets/search.svg";
 import dropdownIcon from "../assets/dropdownIcon.svg";
 import { NavLink } from "react-router-dom";
 import { getCawsNfts, getCawsOldNfts } from "../../../actions/convertUsd";
+import "./_filters.scss";
+import filtersXmark from "./filtersXmark.svg";
+import axios from "axios";
+import { Checkbox } from "@mui/material";
+import OutsideClickHandler from "react-outside-click-handler";
+
 
 const CawsNFT = ({
   isConnected,
@@ -34,8 +40,25 @@ const CawsNFT = ({
   const [favItems, setfavItems] = useState(0);
   const [favorites, setFavorites] = useState([]);
   const [next, setNext] = useState(0);
-
+  const [filters, setFilters] = useState([]);
   const listInnerRef = useRef();
+  const [openTraits, setOpenTraits] = useState(false)
+  const [categoryIndex, setCategoryIndex] = useState(0)
+
+  const fetchFilters = async () => {
+    await axios
+      .get(
+        "https://api.opensea.io/api/v1/collection/catsandwatchessocietycaws",
+        {
+          headers: {
+            "X-API-KEY": "b132fcc52ab540f0b13a319bf57b34f0",
+          },
+        }
+      )
+      .then((res) => {
+        setFilters(res.data.collection.traits);
+      });
+  };
 
   const sortNfts = (sortValue) => {
     if (sortValue === "htl") {
@@ -198,6 +221,7 @@ const CawsNFT = ({
 
   useEffect(() => {
     fetchInitialCaws();
+    fetchFilters();
   }, [next]);
 
   useEffect(() => {
@@ -209,176 +233,224 @@ const CawsNFT = ({
     }
     sortNfts("lth");
   }, [cawsNFTS]);
+
+  console.log(filters)
+
+
   return (
-    <div
-      className="container-fluid d-flex justify-content-end p-0"
-      style={{ minHeight: "72vh", maxWidth: "2400px" }}
-    >
-      {windowSize.width < 992 ? <MobileNav /> : <MarketSidebar />}
-
+    <>
       <div
-        className="container-nft d-flex  align-items-start px-3 px-lg-5 position-relative"
-        style={{ backgroundSize: "cover" }}
-        id="header"
-        onScroll={onScroll}
-        ref={listInnerRef}
+        className="container-fluid d-flex justify-content-end p-0"
+        style={{ minHeight: "72vh", maxWidth: "2400px" }}
       >
-        <div className="container-lg mx-0">
-          <h6 className="nft-page-title font-raleway  pt-4 pt-lg-0 mt-5 mt-lg-4">
-            Cats And Watches Society{" "}
-            <span style={{ color: "#8c56ff" }}>(CAWS)</span>
-          </h6>
-          <div className="d-flex mt-5 mb-3 flex-column flex-lg-row gap-3 gap-lg-0 align-items-start align-items-lg-center justify-content-start justify-content-lg-end">
-            <div class="dropdown" style={{ width: "200px" }}>
-              <button
-                class="btn btn-secondary nft-dropdown w-100
-                 d-flex align-items-center justify-content-between dropdown-toggle"
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                {filterTitle}
-                <img src={dropdownIcon} alt="" />
-              </button>
-              <ul class="dropdown-menu nft-dropdown-menu  p-2 w-100">
-                <li
-                  className="nft-dropdown-item"
-                  onClick={() => {
-                    setFilterTitle("Price low to high");
-                    sortNfts("lth");
-                  }}
-                >
-                  <span>Price low to high</span>
-                </li>
-                <li
-                  className="nft-dropdown-item"
-                  onClick={() => {
-                    setFilterTitle("Price high to low");
-                    sortNfts("htl");
-                  }}
-                >
-                  <span>Price high to low</span>
-                </li>
-                <li
-                  className="nft-dropdown-item"
-                  onClick={() => {
-                    setFilterTitle("Oldest to newest");
-                    sortNfts("otl");
-                  }}
-                >
-                  <span>Oldest to newest</span>
-                </li>
-                <li
-                  className="nft-dropdown-item"
-                  onClick={() => {
-                    setFilterTitle("Newest To Oldest");
-                    sortNfts("lto");
-                  }}
-                >
-                  <span>Newest To Oldest</span>
-                </li>
-                <li
-                  className="nft-dropdown-item"
-                  onClick={() => {
-                    setFilterTitle("Price: ETH");
-                    sortNfts("eth");
-                  }}
-                >
-                  <span>Price: ETH</span>
-                </li>
-                <li
-                  className="nft-dropdown-item"
-                  onClick={() => {
-                    setFilterTitle("Price: DYP");
-                    sortNfts("dyp");
-                  }}
-                >
-                  <span>Price: DYP</span>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="d-flex align-items-center nft-page-wrapper p-4 gap-4 my-4">
-            <div
-              className={
-                loading === false || cawsNFTS.length > 0
-                  ? "item-cards-wrapper"
-                  : "loader-wrapper"
-              }
-            >
-              {cawsNFTS && cawsNFTS.length > 0 && (
-                <>
-                  {cawsNFTS.map((nft, index) => (
-                    <NavLink
-                      to={`/marketplace/nft/${nft.blockTimestamp ?? index}`}
-                      style={{ textDecoration: "none" }}
-                      key={index}
-                      state={{
-                        nft: nft,
-                        type: nft.type,
-                        isOwner:
-                          nft.seller?.toLowerCase() ===
-                            coinbase?.toLowerCase() ||
-                          nft.buyer?.toLowerCase() === coinbase?.toLowerCase(),
-                        chain: nft.chain,
-                      }}
-                      onClick={() => {
-                        updateViewCount(nft.tokenId, nft.nftAddress);
-                      }}
-                    >
-                      <ItemCard
-                        ethTokenData={ethTokenData}
-                        dypTokenData={dypTokenData}
-                        key={nft.id}
-                        nft={nft}
-                        isConnected={isConnected}
-                        showConnectWallet={handleConnect}
-                        isCaws={true}
-                        isTimepiece={false}
-                        isWod={false}
-                        coinbase={coinbase}
-                        isFavorite={
-                          favorites.length > 0
-                            ? favorites.find(
-                                (obj) =>
-                                  obj.nftAddress === nft.nftAddress &&
-                                  obj.tokenId === nft.tokenId
-                              )
-                              ? true
-                              : false
-                            : false
-                        }
-                        onFavorite={updateFavs}
-                      />
-                    </NavLink>
-                  ))}
-                </>
-              )}
+        {windowSize.width < 992 ? <MobileNav /> : <MarketSidebar />}
 
-              {loading === true && (
-                <HashLoader
-                  color={"#554fd8"}
-                  loading={loading}
-                  cssOverride={override}
-                  aria-label="Loading Spinner"
-                  data-testid="loader"
-                />
-              )}
+        <div
+          className="container-nft d-flex  align-items-start px-3 px-lg-5 position-relative"
+          style={{ backgroundSize: "cover" }}
+          id="header"
+          onScroll={onScroll}
+          ref={listInnerRef}
+        >
+          <div className="container-lg mx-0">
+            <h6 className="nft-page-title font-raleway  pt-4 pt-lg-0 mt-5 mt-lg-4">
+              Cats And Watches Society{" "}
+              <span style={{ color: "#8c56ff" }}>(CAWS)</span>
+              <button onClick={() => setOpenTraits(!openTraits)}>open</button>
+            </h6>
+            <div className="d-flex mt-5 mb-3 flex-column flex-lg-row gap-3 gap-lg-0 align-items-start align-items-lg-center justify-content-start justify-content-lg-end">
+              <div class="dropdown" style={{ width: "200px" }}>
+                <button
+                  class="btn btn-secondary nft-dropdown w-100
+                 d-flex align-items-center justify-content-between dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  {filterTitle}
+                  <img src={dropdownIcon} alt="" />
+                </button>
+                <ul class="dropdown-menu nft-dropdown-menu  p-2 w-100">
+                  <li
+                    className="nft-dropdown-item"
+                    onClick={() => {
+                      setFilterTitle("Price low to high");
+                      sortNfts("lth");
+                    }}
+                  >
+                    <span>Price low to high</span>
+                  </li>
+                  <li
+                    className="nft-dropdown-item"
+                    onClick={() => {
+                      setFilterTitle("Price high to low");
+                      sortNfts("htl");
+                    }}
+                  >
+                    <span>Price high to low</span>
+                  </li>
+                  <li
+                    className="nft-dropdown-item"
+                    onClick={() => {
+                      setFilterTitle("Oldest to newest");
+                      sortNfts("otl");
+                    }}
+                  >
+                    <span>Oldest to newest</span>
+                  </li>
+                  <li
+                    className="nft-dropdown-item"
+                    onClick={() => {
+                      setFilterTitle("Newest To Oldest");
+                      sortNfts("lto");
+                    }}
+                  >
+                    <span>Newest To Oldest</span>
+                  </li>
+                  <li
+                    className="nft-dropdown-item"
+                    onClick={() => {
+                      setFilterTitle("Price: ETH");
+                      sortNfts("eth");
+                    }}
+                  >
+                    <span>Price: ETH</span>
+                  </li>
+                  <li
+                    className="nft-dropdown-item"
+                    onClick={() => {
+                      setFilterTitle("Price: DYP");
+                      sortNfts("dyp");
+                    }}
+                  >
+                    <span>Price: DYP</span>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
-          {!loading && next < allCaws && (
-            <div className="d-flex justify-content-center w-100">
-              <button
-                className="btn py-2 px-3 nft-load-more-btn"
-                onClick={() => loadMore()}
+            <div className="d-flex align-items-center nft-page-wrapper p-4 gap-4 my-4">
+              <div
+                className={
+                  loading === false || cawsNFTS.length > 0
+                    ? "item-cards-wrapper"
+                    : "loader-wrapper"
+                }
               >
-                Load more
-              </button>
+                {cawsNFTS && cawsNFTS.length > 0 && (
+                  <>
+                    {cawsNFTS.map((nft, index) => (
+                      <NavLink
+                        to={`/marketplace/nft/${nft.blockTimestamp ?? index}`}
+                        style={{ textDecoration: "none" }}
+                        key={index}
+                        state={{
+                          nft: nft,
+                          type: nft.type,
+                          isOwner:
+                            nft.seller?.toLowerCase() ===
+                              coinbase?.toLowerCase() ||
+                            nft.buyer?.toLowerCase() ===
+                              coinbase?.toLowerCase(),
+                          chain: nft.chain,
+                        }}
+                        onClick={() => {
+                          updateViewCount(nft.tokenId, nft.nftAddress);
+                        }}
+                      >
+                        <ItemCard
+                          ethTokenData={ethTokenData}
+                          dypTokenData={dypTokenData}
+                          key={nft.id}
+                          nft={nft}
+                          isConnected={isConnected}
+                          showConnectWallet={handleConnect}
+                          isCaws={true}
+                          isTimepiece={false}
+                          isWod={false}
+                          coinbase={coinbase}
+                          isFavorite={
+                            favorites.length > 0
+                              ? favorites.find(
+                                  (obj) =>
+                                    obj.nftAddress === nft.nftAddress &&
+                                    obj.tokenId === nft.tokenId
+                                )
+                                ? true
+                                : false
+                              : false
+                          }
+                          onFavorite={updateFavs}
+                        />
+                      </NavLink>
+                    ))}
+                  </>
+                )}
+
+                {loading === true && (
+                  <HashLoader
+                    color={"#554fd8"}
+                    loading={loading}
+                    cssOverride={override}
+                    aria-label="Loading Spinner"
+                    data-testid="loader"
+                  />
+                )}
+              </div>
             </div>
-          )}
-        </div>
+            {!loading && next < allCaws && (
+              <div className="d-flex justify-content-center w-100">
+                <button
+                  className="btn py-2 px-3 nft-load-more-btn"
+                  onClick={() => loadMore()}
+                >
+                  Load more
+                </button>
+              </div>
+            )}
+          </div>
       </div>
+      </div>
+      <OutsideClickHandler onOutsideClick={() => setOpenTraits(false)}>
+      <div className={`filters-wrapper col-12 col-md-10 col-lg-8 col-xxl-5 ${openTraits && "filters-active"} p-4`}>
+      <div className="d-flex align-items-center justify-content-between mb-4">
+        <h6 className="filters-title mb-0">Filters</h6>
+        <img src={filtersXmark} style={{cursor: 'pointer'}} onClick={() => setOpenTraits(false)} alt="" />
+      </div>
+      <div className="d-flex align-items-center justify-content-between mb-4">
+        <span className="select-category mb-0">Select Category</span>
+        <span className="clear-all mb-0">Clear all</span>
+      </div>
+      <div className="filter-tabs d-flex align-items-center justify-content-start gap-4">
+        {filters &&
+        Object.entries(filters).map(([key, value], i) => (
+          <div className={`filter-tab px-2 py-1 d-flex align-items-center ${categoryIndex === i && "filter-tab-active"}`} onClick={() => setCategoryIndex(i)} key={i}>
+            <h6 className="filter-tab-title mb-0">{key} ({Object.keys(value)?.length})</h6>
+          </div>
+        ))}
+      </div>
+      <span className="filters-divider my-4"></span>
+        <div className="row align-items-center traits-wrapper" style={{rowGap: '20px'}}>
+          {Object.values(filters)[categoryIndex] && Object.values(filters) && Object.entries(Object.values(filters)[categoryIndex]).map(([key, value], i) => (
+            // <span key={i}>{key} ({value})</span>
+            <div className="col-12 col-md-6 col-lg-4 col-xxl-3">
+              <div className="trait-wrapper d-flex align-items-center justify-content-between  px-2">
+              <div className="d-flex align-items-center">
+                <Checkbox  sx={{
+              color: "#8E97CD",
+              "&.Mui-checked": {
+                color: "#82DAAB",
+              },
+            }} />
+                <span className="trait-title mb-0">{key}</span>
+              </div>
+              <span className="trait-amount mb-0">{value}</span>
+            </div>
+            </div>
+          ))}
+          </div>
     </div>
+      </OutsideClickHandler>
+    </>
   );
 };
 
