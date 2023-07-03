8ddf6e0b31465c7e609f9fb89b1cfa9ea40ceaf5
LorenaL579 <lorena.lici579@gmail.com>
2023-07-03 12:00:13 +0200

    fixes for modal

--- src/screens/Marketplace/MarketNFTs/ConfirmationModal.js
+++ src/screens/Marketplace/MarketNFTs/ConfirmationModal.js
@@ -7,6 +7,8 @@ import successImg from "./assets/success.svg";
 import "./_filters.scss";
 import getFormattedNumber from "../../Caws/functions/get-formatted-number";
 import OutsideClickHandler from "react-outside-click-handler";
+import useWindowSize from "../../../hooks/useWindowSize";
+
 const ConfirmationModal = ({
   open,
   onclose,
@@ -18,12 +20,15 @@ const ConfirmationModal = ({
   ethTokenData,
   dypTokenData,
 }) => {
+
+  const windowSize = useWindowSize();
+
   const style = {
     position: "absolute",
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: "30%",
+    width: windowSize.width > 1400 ? "30%" : windowSize.width > 786 ? "50%" : "90%",
     boxShadow: 24,
     p: 4,
     overflow: "auto",
@@ -56,8 +61,8 @@ const ConfirmationModal = ({
                 />
               </div>
               <div className="summarywrapper">
-                <div className="d-flex align-items-center justify-content-between">
-                  <div className="d-flex align-items-center gap-2">
+                <div className="d-flex flex-column flex-column flex-xxl-row flex-lg-row align-items-center justify-content-between">
+                  <div className="d-flex flex-column w-100 flex-xxl-row flex-lg-row align-items-center gap-2">
                     <img
                       className="p-0 nft-img"
                       src={
@@ -68,10 +73,10 @@ const ConfirmationModal = ({
                           : `https://timepiece.worldofdypians.com/thumbs150/${nft.tokenId}.png`
                       }
                       alt=""
-                      style={{ width: 80, height: 80, borderRadius: 20 }}
+                      style={{ width: windowSize.width > 500 ? 80 : '100%', height: windowSize.width > 500 ? 80 : '150', borderRadius: 20 }}
                     />
                     <div className="d-flex flex-column justify-content-between">
-                      <div className="d-flex flex-column">
+                      <div className="d-flex flex-column align-items-center">
                         <span className="itemname">
                           {isCaws
                             ? "CAWS"
@@ -87,8 +92,8 @@ const ConfirmationModal = ({
                       <span className="itemchain">Chain: Ethereum</span>
                     </div>
                   </div>
-                  <div className="d-flex flex-column">
-                    <span className="itemname">
+                  <div className="d-flex flex-row flex-lg-column flex-xxl-column gap-2 gap-lg-0 gap-xxl-0 align-items-center">
+                    <span className="itemname" style={{whiteSpace: 'nowrap'}}>
                       {getFormattedNumber(nft.price / 1e18, 2)}{" "}
                       {nft.payment_priceType === 0 ? "ETH" : "DYP"}
                     </span>
@@ -132,8 +137,8 @@ const ConfirmationModal = ({
                 />
               </div>
               <div className="summarywrapper">
-                <div className="d-flex align-items-center justify-content-between">
-                  <div className="d-flex align-items-center gap-2">
+                <div className="d-flex flex-column flex-column flex-xxl-row flex-lg-row align-items-center justify-content-between">
+                  <div className="d-flex flex-column w-100 flex-xxl-row flex-lg-row align-items-center gap-2">
                     <img
                       className="p-0 nft-img"
                       src={
@@ -144,9 +149,9 @@ const ConfirmationModal = ({
                           : `https://timepiece.worldofdypians.com/thumbs150/${nft.tokenId}.png`
                       }
                       alt=""
-                      style={{ width: 80, height: 80, borderRadius: 20 }}
+                      style={{ width: windowSize.width > 500 ? 80 : '100%', height: windowSize.width > 500 ? 80 : '150', borderRadius: 20 }}
                     />
-                    <div className="d-flex flex-column justify-content-between">
+                    <div className="d-flex flex-column align-items-center">
                       <div className="d-flex flex-column">
                         <span className="itemname">
                           {isCaws
@@ -160,8 +165,8 @@ const ConfirmationModal = ({
                       <span className="itemchain">Chain: Ethereum</span>
                     </div>
                   </div>
-                  <div className="d-flex flex-column">
-                    <span className="itemname">
+                  <div className="d-flex flex-row flex-lg-column flex-xxl-column gap-2 gap-lg-0 gap-xxl-0 align-items-center">
+                    <span className="itemname" style={{whiteSpace: 'nowrap'}}>
                       {getFormattedNumber(nft.price / 1e18, 2)}{" "}
                       {nft.payment_priceType === 0 ? "ETH" : "DYP"}
                     </span>
