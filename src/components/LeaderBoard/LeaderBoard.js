7b36e20465012837709b9bbb1e5e761b7c8c492e
LorenaL579 <lorena.lici579@gmail.com>
2023-07-10 09:09:36 +0200

    updated previous weekly golden rewards

--- src/components/LeaderBoard/LeaderBoard.js
+++ src/components/LeaderBoard/LeaderBoard.js
@@ -578,7 +578,7 @@ const LeaderBoard = ({ username, userId, dypBalancebnb, address }) => {
     } else if (item === "weekly" && inactiveBoard === false) {
       setPrizes(weeklyPrizes);
     } else if (item === "weekly" && inactiveBoard === true) {
-      setPrizes(previous_weeklyPrizes);
+      setPrizes(weeklyPrizes);
     } else if (item === "monthly" && inactiveBoard === false) {
       setPrizes(monthlyPrizes);
     } else if (item === "monthly" && inactiveBoard === true) {
@@ -997,7 +997,11 @@ const LeaderBoard = ({ username, userId, dypBalancebnb, address }) => {
                             : "goldenscore-inactive"
                         }`}
                       >
-                         +${getFormattedNumber(previous_monthlyPrizesGolden[index], 0)}
+                        +$
+                        {getFormattedNumber(
+                          previous_monthlyPrizesGolden[index],
+                          0
+                        )}
                       </td>
                     </tr>
                   );
@@ -1058,7 +1062,7 @@ const LeaderBoard = ({ username, userId, dypBalancebnb, address }) => {
                             : "goldenscore-inactive"
                         }`}
                       >
-                        +${getFormattedNumber(previous_weeklyPrizesGolden[index], 0)}
+                        +${getFormattedNumber(weeklyPrizesGolden[index], 0)}
                       </td>
                     </tr>
                   );
