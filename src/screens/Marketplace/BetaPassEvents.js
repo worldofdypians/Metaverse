95aa28789450007847295b3b3ee06c94f4d839e5
LorenaL579 <lorena.lici579@gmail.com>
2023-09-07 15:34:21 +0200

    added navlink

--- src/screens/Marketplace/BetaPassEvents.js
+++ src/screens/Marketplace/BetaPassEvents.js
@@ -1,27 +1,35 @@
 import React from "react";
 import greenArrow from "./assets/greenArrow.svg";
+import { NavLink } from "react-router-dom";
 
 const BetaPassEvents = () => {
   return (
     <div class="upcoming-mint-wrapper d-flex flex-column flex-lg-row align-items-center justify-content-between px-0">
-      <div class="d-flex flex-column p-3">
-        <h6 class="upcoming-mint-title">Treasure Hunt</h6>
-        <p class="upcoming-mint-desc">
-          Discover hidden treasures and local partner rewards in our exciting
-          Treasure Hunt event!
-        </p>
-        <span
-          className="detailsgreen-txt d-flex align-items-center gap-1"
-          style={{ width: "fit-content" }}
-        >
-          Details <img src={greenArrow} alt="" />
-        </span>
-      </div>
-      <img
-        src={require("./assets/treasurebg.png")}
-        alt=""
-        class="upcoming-mint-img d-flex"
-      />
+      <NavLink
+        to=""
+        className={
+          "d-flex flex-column flex-lg-row align-items-center justify-content-between px-0 w-100"
+        }
+      >
+        <div class="d-flex flex-column p-3">
+          <h6 class="upcoming-mint-title">Treasure Hunt</h6>
+          <p class="upcoming-mint-desc">
+            Discover hidden treasures and local partner rewards in our exciting
+            Treasure Hunt event!
+          </p>
+          <span
+            className="detailsgreen-txt d-flex align-items-center gap-1"
+            style={{ width: "fit-content" }}
+          >
+            Details <img src={greenArrow} alt="" />
+          </span>
+        </div>
+        <img
+          src={require("./assets/treasurebg.png")}
+          alt=""
+          class="upcoming-mint-img d-flex"
+        />
+      </NavLink>
     </div>
   );
 };
