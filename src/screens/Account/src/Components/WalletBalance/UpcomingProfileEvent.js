b2748626b406bd56b08f216b507c970f50a1a946
LorenaL579 <lorena.lici579@gmail.com>
2023-09-13 11:15:05 +0200

    fixes for popup

--- src/screens/Account/src/Components/WalletBalance/UpcomingProfileEvent.js
+++ src/screens/Account/src/Components/WalletBalance/UpcomingProfileEvent.js
@@ -1,52 +1,72 @@
-import React from 'react'
+import React from "react";
 import coin98 from "./assets/coin98.svg";
 import coingecko from "./assets/coingecko.svg";
 import conflux from "./assets/conflux.svg";
-import purpleArrow from "./assets/purpleArrow.svg";
-import purpleDate from "./assets/purpleDate.svg";
-import purpleDollar from "./assets/purpleDollar.svg";
-import purpleExplore from "./assets/purpleExplore.svg";
+import orangeArrow from "./assets/orangeArrow.svg";
+
+import orangeDate from "./assets/orangeDate.svg";
+
+import orangeExplore from "./assets/orangeExplore.svg";
+
 import purpleFind from "./assets/purpleFind.svg";
 import confluxProfileBanner from "./assets/confluxProfileBanner.png";
 import coingeckoProfileBanner from "./assets/coingeckoProfileBanner.png";
 import coingeckoLogo from "./assets/coingeckoLogo.svg";
 
-const UpcomingProfileEvent = ({onOpenEvent, data}) => {
+const UpcomingProfileEvent = ({ onOpenEvent, data }) => {
   return (
-    <div className="profile-event-item d-flex flex-column position-relative" onClick={onOpenEvent} style={{background: "rgba(140, 86, 255, 0.10)", transform: 'translateX(0px)', borderBottom: "1px solid #8C56FF"}}>
-    <div className="profile-event-tag d-flex align-items-center justify-content-center px-1" style={{background: "#8C56FF"}}> 
-    <span className="profile-event-tag-text mb-0" style={{color: "#FFFFFF"}}>Coming Soon</span>
-    </div>
-    <div className="profile-event-top d-flex align-items-center justify-content-between">
-      <div className="d-flex align-items-center p-2 gap-2">
-        <img src={coingeckoLogo} height={16} width={16} alt="" />
-        <div className="d-flex flex-column ">
-          <h6 className="profile-event-title mb-0">CoinGecko Pass</h6>
-          <span className="profile-event-rewards mb-0">
-            $5,000 in BNB rewards
+    <div
+      className="profile-event-item d-flex flex-column position-relative"
+      onClick={onOpenEvent}
+      style={{
+        background: "rgba(236, 129, 35, 0.10)",
+        transform: "translateX(0px)",
+        borderBottom: "1px solid rgba(236, 129, 35, 1)",
+      }}
+    >
+      <div className="profile-event-top d-flex align-items-center justify-content-between">
+        <div className="d-flex align-items-center p-2 gap-2">
+          <img src={coingeckoLogo} height={16} width={16} alt="" />
+          <div className="d-flex flex-column ">
+            <h6 className="profile-event-title mb-0 d-flex align-items-center gap-1">
+              CoinGecko
+              <div
+                className="profile-event-tag position-relative d-flex align-items-center justify-content-center px-1"
+                style={{ background: "#EC8123", top: 0, right: 0 }}
+              >
+                <span
+                  className="profile-event-tag-text mb-0"
+                  style={{ color: "#FFFFFF" }}
+                >
+                  Coming Soon
+                </span>
+              </div>
+            </h6>
+            <span className="profile-event-rewards mb-0">
+              $3,000 in BNB rewards
+            </span>
+          </div>
+        </div>
+        <img src={coingeckoProfileBanner} style={{ height: "50px" }} alt="" />
+      </div>
+      <div className="profile-event-bottom p-2 d-flex align-items-center justify-content-between">
+        <div className="d-flex align-items-center gap-1">
+          <img src={orangeExplore} height={15} width={15} alt="" />
+          <span className="mb-0 event-bottom-text" style={{ color: "#EC8123" }}>
+            Explore & Mine
           </span>
         </div>
-      </div>
-      <img src={coingeckoProfileBanner} style={{height: "50px"}} alt=""  />
-    </div>
-    <div className="profile-event-bottom p-2 d-flex align-items-center justify-content-between" >
-      <div className="d-flex align-items-center gap-1">
-        <img src={purpleExplore} height={15} width={15} alt="" />
-        <span className="mb-0 event-bottom-text" style={{color: "#8C56FF"}}>
-          Explore & Mine
-        </span>
-      </div>
-     
-      <div className="d-flex align-items-center gap-1">
-        <img src={purpleDate} height={15} width={15} alt="" />
-        <span className="mb-0 event-bottom-text" style={{color: "#8C56FF"}}>
-          Sept 25, 2023
-        </span>
-      </div>
-      <img src={purpleArrow} height={15} width={15} alt="" />
-    </div>
-  </div>
-  )
-}
 
-export default UpcomingProfileEvent
+        <div className="d-flex align-items-center gap-1">
+          <img src={orangeDate} height={15} width={15} alt="" />
+          <span className="mb-0 event-bottom-text" style={{ color: "#EC8123" }}>
+            Sept 25, 2023
+          </span>
+        </div>
+        <img src={orangeArrow} height={15} width={15} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default UpcomingProfileEvent;
