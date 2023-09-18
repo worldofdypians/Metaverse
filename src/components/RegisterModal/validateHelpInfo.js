4a22bdf86521a687fe6ea6cea05c9c249a677bc5
LorenaL579 <lorena.lici579@gmail.com>
2023-09-18 12:24:37 +0200

    fix for discord username

--- src/components/RegisterModal/validateHelpInfo.js
+++ src/components/RegisterModal/validateHelpInfo.js
@@ -8,8 +8,6 @@ export default function validateHelpInfo(values) {
   }
   if (!values.discord.trim()) {
     errors.discord = "Discord username is Required";
-  } else if (!/^@?[^#@:]{2,32}#[0-9]{4}/i.test(values.discord)) {
-    errors.discord = "Discord username is Invalid";
   }
   return errors;
 }
