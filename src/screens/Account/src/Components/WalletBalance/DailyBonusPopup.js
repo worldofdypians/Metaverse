b4a50ca21219bc379955a08f171884fa2fd25c99
LorenaL579 <lorena.lici579@gmail.com>
2023-10-13 12:11:51 +0200

    added bottom part of modal

--- src/screens/Account/src/Components/WalletBalance/DailyBonusPopup.js
+++ src/screens/Account/src/Components/WalletBalance/DailyBonusPopup.js
@@ -1,19 +1,12 @@
 import React from "react";
-import OutsideClickHandler from "react-outside-click-handler";
-import chestLocked from "./assets/chest-locked.webp";
-import chestUnLocked from "./assets/chest-unlocked.webp";
-import hexagon from "./assets/hexagon.svg";
-import chestOpen from './assets/chestOpen.png'
-import chestClosed from './assets/chestClosed.png'
-import chestBgOpen from './assets/chestBgOpen.svg'
-import chestBgClosed from './assets/chestBgClosed.svg'
-import chestTagOpen from './assets/chestTagOpen.svg'
-import chestTagClosed from './assets/chestTagClosed.svg'
+import chestOpen from "./assets/chestOpen.png";
+import chestClosed from "./assets/chestClosed.png";
 import closeX from "./assets/closeX.svg";
+import pointsLogo from "./assets/pointslogo.png";
+import rewardsLogo from "./assets/rewardslogo.png";
 
-const DailyBonusPopup = ({onclose}) => {
-
-  const dummyChests = [ 
+const DailyBonusPopup = ({ onclose }) => {
+  const dummyChests = [
     {
       open: true,
       chestTitle: "Chest #1",
@@ -64,35 +57,73 @@ const DailyBonusPopup = ({onclose}) => {
       chestTitle: "Chest #10",
       chestId: 10,
     },
-    
-  ]
-
+  ];
 
   return (
     <>
       <div className="package-popup2 dragon-popup px-4 py-5 py-lg-5 px-lg-5">
-        <img src={closeX} alt="" className="closex position-absolute" onClick={onclose}/>
+        <img
+          src={closeX}
+          alt=""
+          className="closex position-absolute"
+          onClick={onclose}
+        />
         <div className="position-relative h-100">
           <div className="overlay-container">
             <div className="d-flex flex-column">
               <span className="bonustitle position-relative">Daily Bonus</span>
             </div>
           </div>
-          <div className="positon-relative h-100 d-flex">
+          <div className="positon-relative h-100 d-flex flex-column gap-3">
             <div className="rewardsgrid">
-                {dummyChests.map((item) => (
-                  <div className={` reward-chest ${item.open ? "reward-chest-open" : "reward-chest-closed" } position-relative d-flex flex-column align-items-center justify-content-center gap-2`}>
-                  <div className={`chest-number ${item.open ? "number-open" : "number-closed"} d-flex align-items-center justify-content-center`}>
+              {dummyChests.map((item) => (
+                <div
+                  className={` reward-chest ${
+                    item.open ? "reward-chest-open" : "reward-chest-closed"
+                  } position-relative d-flex flex-column align-items-center justify-content-center gap-2`}
+                >
+                  <div
+                    className={`chest-number ${
+                      item.open ? "number-open" : "number-closed"
+                    } d-flex align-items-center justify-content-center`}
+                  >
                     <span className="chest-number-text mb-0">
-                    {item.chestId}
+                      {item.chestId}
                     </span>
                   </div>
-                  <img src={item.open ? chestOpen : chestClosed} className="chest-image" alt="" />
-                  <h6 className="chest-title mb-0">
-                    {item.chestTitle}
-                  </h6>
+                  <img
+                    src={item.open ? chestOpen : chestClosed}
+                    className="chest-image"
+                    alt=""
+                  />
+                  <h6 className="chest-title mb-0">{item.chestTitle}</h6>
                 </div>
-                ))}
+              ))}
+            </div>
+            <button className="">Claim</button>
+            <div className="dailyreward-separator"></div>
+            <div className="d-flex flex-column gap-2">
+              <span className="font-organetto chestprizetitle text-white">CHEST PRIZES</span>
+              <div className="d-flex align-items-center gap-2">
+                <div className="prizeswrapper">
+                  <div className="d-flex align-items-center gap-2">
+                    <img src={pointsLogo} alt="" style={{width: 40, height: 40}}/>
+                    <span className="text-white">10,000 points</span>
+                  </div>
+                </div>
+                <div className="prizeswrapper">
+                  <div className="d-flex align-items-center gap-2">
+                    <img src={rewardsLogo} alt="" style={{width: 40, height: 40}}/>
+                    <span className="text-white">$2.5 Rewards</span>
+                  </div>
+                </div>
+                <div className="prizeswrapper">
+                  <div className="d-flex align-items-center gap-2">
+                    <img src={rewardsLogo} alt="" style={{width: 40, height: 40}}/>
+                    <span className="text-white">1 Genesis land Nft</span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
