ddf9d4d05eb5237af96049dfee5a07c4df083cc3
LorenaL579 <lorena.lici579@gmail.com>
2023-11-10 12:54:43 +0100

    updated new dyp address

--- src/screens/Account/src/web3/index.js
+++ src/screens/Account/src/web3/index.js
@@ -16,7 +16,7 @@ const cawsContractAddress = "0xd06cF9e1189FEAb09c844C597abc3767BC12608c";
 const cawsStakeContractAddress = "0xEe425BbbEC5e9Bf4a59a1c19eFff522AD8b7A47A";
 const TopUpContractAddress = "0xAdD0F01275080E18975bf1C70ce213ad48DE24bf";
 const erc20Address = "0x961C8c0B1aaD0c0b10a51FeF6a867E3091BCef17";
-const token_address = "0x961c8c0b1aad0c0b10a51fef6a867e3091bcef17";
+const token_address = "0x1a3264F2e7b1CFC6220ec9348d33cCF02Af7aaa4";
 const idyptoken_address = "0xBD100d061E120b2c67A24453CF6368E63f1Be056";
 
 const wod_address = "0xc40bE3A801A39bdC151BF6b3468B4035F8A4d440";
