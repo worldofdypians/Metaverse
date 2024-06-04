1fe374544fc80dbb92d74aa5a7630685311cdc9e
LorenaL57 <lorena.lici579@gmail.com>
2024-06-04 10:49:13 +0200

    added core giveaway popup

--- src/components/TimepieceMint/MintPopup.js
+++ src/components/TimepieceMint/MintPopup.js
@@ -63,6 +63,16 @@ const MintPopup = ({ active, onClose, data }) => {
           </div>
           <h6 className="mint-popup-title">DogeCoin Beta Pass</h6>
         </>
+      ): data.title === "CORE" ? (
+        <>
+          <div className="d-flex align-items-center justify-content-center">
+            <div className="pulsatingDot"></div>
+            <h6 className="mint-popup-title  mb-0" style={{ color: "#18FFFF" }}>
+              Live Giveaway
+            </h6>
+          </div>
+          <h6 className="mint-popup-title">CORE Beta Pass NFT</h6>
+        </>
       ) : data.title === "CoinMarketCap" ? (
         <>
           <div className="d-flex align-items-center justify-content-center">
@@ -149,6 +159,15 @@ const MintPopup = ({ active, onClose, data }) => {
             </span>
           )}
         </div>
+      ) : data.title === "CORE" ? (
+        <div className="available-mint-bg d-flex align-items-center justify-content-center px-2 py-1">
+          {data.title === "CORE" && (
+            <span className="popup-available-mint">
+              Get access to the game and a unique event filled with surprises
+              and rewards!
+            </span>
+          )}
+        </div>
       ): data.title === "CoinMarketCap" ? (
         <div className="available-mint-bg d-flex align-items-center justify-content-center px-2 py-1">
           {data.title === "CoinMarketCap" && (
@@ -184,6 +203,15 @@ const MintPopup = ({ active, onClose, data }) => {
           <Link
             onClick={onClose}
             to={"/marketplace/beta-pass/doge"}
+            state={{ event: data.state }}
+            className="linear-border"
+          >
+            <button className="btn filled-btn px-4">More</button>
+          </Link>
+        ) : data.title === "CORE" ? (
+          <Link
+            onClick={onClose}
+            to={"/marketplace/beta-pass/core"}
             state={{ event: data.state }}
             className="linear-border"
           >
