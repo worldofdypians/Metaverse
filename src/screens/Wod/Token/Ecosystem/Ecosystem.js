7a9893c54bd4242167390f13ed772d161cdc7e9f
LorenaL57 <lorena.lici579@gmail.com>
2024-06-06 10:44:04 +0200

    fixes for ecosystem

--- src/screens/Wod/Token/Ecosystem/Ecosystem.js
+++ src/screens/Wod/Token/Ecosystem/Ecosystem.js
@@ -3,8 +3,8 @@ import "./_ecosystem.scss";
 
 const Ecosystem = () => {
   return (
-    <div className="ecosystem-wrapper position-relative d-flex align-items-center">
-      <div className="container-lg">
+    <div className="ecosystem-wrapper position-relative d-flex justify-content-center align-items-center">
+      <div className="custom-container">
         <h4 className="main-hero-title font-montserrat text-center">
           WoD{" "}
           <mark className="font-montserrat main-hero-title explore-tag pe-2">
