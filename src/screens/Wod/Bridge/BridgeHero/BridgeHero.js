4e7e3ebc1d65f19401a8a41f0226e7797beb6483
LorenaL57 <lorena.lici579@gmail.com>
2024-06-06 12:11:28 +0200

    updates about wod bridge

---
+++ src/screens/Wod/Bridge/BridgeHero/BridgeHero.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./_bridgehero.scss";
+
+const BridgeHero = () => {
+  return (
+    <div className="ecosystem-wrapper position-relative d-flex justify-content-center align-items-center">
+      <div className="custom-container w-100">
+        <div className="bridge-hero-wrapper p-3">
+        <div className="d-flex flex-column gap-4">bridge hero</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BridgeHero;
