3a5764abf6e9347a1125b59d866301b305f22b11
AldiAlinj <aldialinj0@gmail.com>
2024-06-18 15:01:01 +0200

    started about main hero

--- src/screens/About/MainHero/MainHero.js
+++ src/screens/About/MainHero/MainHero.js
@@ -1,10 +1,59 @@
 import React from "react";
+import "./_mainheroabout.scss";
+import epicblack from "../../../assets/epicblack.svg";
+import anchorArrow from "../../../assets/anchorArrow.svg";
 
 const MainHero = () => {
   return (
-    <div className="mainhero-wrapper video-wrapper position-relative d-flex align-items-center flex-column justify-content-end gap-5">
+    <div className="mainhero-wrapper about-wrapper video-wrapper position-relative d-flex align-items-center flex-column justify-content-center gap-5 position-relative">
       <div className="custom-container  mt-5 mt-lg-0">
-        <div className="d-flex flex-column w-100">mainhero about</div>
+        <div className="d-flex w-100 flex-column align-items-center gap-2">
+          <h2 className="font-montserrat builders-title explorer-grid-title px-0">
+            The Biggest metaverse ever built
+          </h2>
+          <p className="about-hero-desc">
+            An action-packed multiplayer game where heroes come together to
+            defend their planet against the Dark Lord's invasion. Choose from a
+            variety of heroes, each with unique abilities, and join forces with
+            other players to destroy the enemy's bases.
+          </p>
+          <div className="opacitywrapper5 filled-btn m-0 px-3">
+            <a
+              className="game-event-download py-1 d-flex align-items-center gap-2"
+              href="https://store.epicgames.com/p/world-of-dypians-2e0694"
+              target="_blank"
+            >
+              <img src={epicblack} alt="icon" className="epicgame2" />
+              Download
+            </a>
+          </div>
+        </div>
+        <div className="anchors-wrapper py-3 w-100 d-flex align-items-center justify-content-center gap-5">
+          <div className="d-flex align-items-center gap-2">
+            <span className="anchor-title">Partners</span>
+            <img src={anchorArrow} alt="" />
+          </div>
+          <div className="d-flex align-items-center gap-2">
+            <span className="anchor-title">Roadmap</span>
+            <img src={anchorArrow} alt="" />
+          </div>
+          <div className="d-flex align-items-center gap-2">
+            <span className="anchor-title">Tokenomics</span>
+            <img src={anchorArrow} alt="" />
+          </div>
+          <div className="d-flex align-items-center gap-2">
+            <span className="anchor-title">Security</span>
+            <img src={anchorArrow} alt="" />
+          </div>
+          <div className="d-flex align-items-center gap-2">
+            <span className="anchor-title">Team</span>
+            <img src={anchorArrow} alt="" />
+          </div>
+          <div className="d-flex align-items-center gap-2">
+            <span className="anchor-title">Brand</span>
+            <img src={anchorArrow} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
