100c67b8f14477edb729e931dcf482f7ee27ba26
LorenaL57 <lorena.lici579@gmail.com>
2024-06-21 11:19:40 +0200

    started governance

--- src/screens/Community/Governance/Governance.js
+++ src/screens/Community/Governance/Governance.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import './_governance.scss'
-
-
+import React from "react";
+import "./_governance.scss";
+import GovernanceContent from "./GovernanceContent/GovernanceContent";
+import GovernanceHero from "./GovernanceHero/GovernanceHero";
 const Governance = () => {
   return (
-    <div className="d-flex justify-content-center w-100">
-    <h1 className='font-organetto text-white'>Governance</h1>
-</div>
-  )
-}
+    <div className="container-fluid px-0">
+      <div className="d-flex flex-column gap-5"> 
+      <GovernanceHero/>
+      <GovernanceContent/>
+    </div>
+    </div>
+  );
+};
 
-export default Governance
+export default Governance;
