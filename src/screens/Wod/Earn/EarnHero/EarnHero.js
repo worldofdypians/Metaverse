2e66d05929b2029411780fd1582614995daa582f
LorenaL57 <lorena.lici579@gmail.com>
2024-07-03 12:48:32 +0200

    updated main hero

--- src/screens/Wod/Earn/EarnHero/EarnHero.js
+++ src/screens/Wod/Earn/EarnHero/EarnHero.js
@@ -1,7 +1,8 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Slider from "react-slick";
 import { NavLink } from "react-router-dom";
 import BetaEventCardHome from "../../../Marketplace/components/BetaEventCardHome";
+import dropdownIcon from "../assets/dropdownIcon.svg";
 
 const EarnHero = () => {
   const dypProducts = [
@@ -78,10 +79,13 @@ const EarnHero = () => {
     ],
   };
 
+  const [filterTitle, setFilterTitle] = useState("Filter");
+
   const betaSlider = useRef(null);
 
   return (
-    <div className="mainhero-wrapper video-wrapper position-relative d-flex align-items-center flex-column justify-content-end gap-5">
+    <div className="earn-mainhero-wrapper video-wrapper position-relative d-flex align-items-center flex-column justify-content-end gap-5">
+      
       <div className="custom-container  mt-5 mt-lg-0">
         <div className="d-flex flex-column w-100">
           <div className="row mx-0 align-items-center justify-content-between gap-2 mt-5 mt-lg-0">
@@ -122,9 +126,64 @@ const EarnHero = () => {
             <div className="container-fluid py-4 buy-wod-bg">
               <div className="custom-container p-0">
                 <div className="d-flex flex-column flex-lg-row align-items-center gap-4">
-                   
                   <div className="row w-100 mx-0 align-items-center justify-content-between">
-                   
+                    <div className="dropdown filters-dropdown">
+                      <button
+                        className="btn btn-secondary nft-dropdown w-100
+                 d-flex align-items-center justify-content-between dropdown-toggle"
+                        type="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        <div className="d-flex align-items-center gap-2">
+                          <h6 className="filter-nav-title mb-0">
+                            {filterTitle}
+                          </h6>
+                        </div>
+                        <img src={dropdownIcon} alt="" />
+                      </button>
+                      <ul className="dropdown-menu nft-dropdown-menu  p-2 w-100">
+                        <li
+                          className="nft-dropdown-item"
+                          onClick={() => {
+                            setFilterTitle("All pools");
+                          }}
+                        >
+                          <span>All pools</span>
+                        </li>
+                        <li
+                          className="nft-dropdown-item"
+                          onClick={() => {
+                            setFilterTitle("Token");
+                          }}
+                        >
+                          <span>Token</span>
+                        </li>
+                        <li
+                          className="nft-dropdown-item"
+                          onClick={() => {
+                            setFilterTitle("NFT");
+                          }}
+                        >
+                          <span>NFT</span>
+                        </li>
+                        <li
+                          className="nft-dropdown-item"
+                          onClick={() => {
+                            setFilterTitle("Token + NFT");
+                          }}
+                        >
+                          <span>Token + NFT</span>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="tvl-earn-wrapper p-2">
+                      <div className="d-flex align-items-center gap-2">
+                        <span className="tvl-earn-title">TVL</span>
+                        <span className="tvl-earn-amount">$1,6000,000+</span>
+
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
