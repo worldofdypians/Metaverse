88ac1484c817716b646fce0f0e2c078d067f5c97
LorenaL57 <lorena.lici579@gmail.com>
2024-07-04 17:15:51 +0200

    updates about governance, added create proposal popup and past proposals

--- src/screens/Community/Governance/Governance.js
+++ src/screens/Community/Governance/Governance.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./_governance.scss";
 import GovernanceContent from "./GovernanceContent/GovernanceContent";
 import GovernanceHero from "./GovernanceHero/GovernanceHero";
+import CreateProposal from "./CreateProposal/CreateProposal";
+
 const Governance = () => {
+  const [createProposalPopup, setCreateProposalPopup] = useState(false);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <div className="container-fluid px-0">
-      <div className="d-flex flex-column gap-5"> 
-      <GovernanceHero/>
-      <GovernanceContent/>
-    </div>
-    </div>
+    <>
+      <div className="container-fluid px-0">
+        <div className="d-flex flex-column gap-5">
+          <GovernanceHero
+            onCreateProposal={() => {
+              setCreateProposalPopup(true);
+            }}
+          />
+          <GovernanceContent />
+        </div>
+      </div>
+      {createProposalPopup && (
+        <CreateProposal
+          onClose={() => {
+            setCreateProposalPopup(false);
+          }}
+          open={createProposalPopup}
+        />
+      )}
+    </>
   );
 };
 
