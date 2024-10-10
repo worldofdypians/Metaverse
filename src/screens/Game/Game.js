43f91a293c1bb1d5532f93783c58fd43359134f9
LorenaL57 <lorena.lici579@gmail.com>
2024-10-10 17:10:30 +0200

    finished popups

--- src/screens/Game/Game.js
+++ src/screens/Game/Game.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./_game.scss";
 import GameHero from "./GameHero";
 import ClassSelection from "./ClassSelection";
@@ -6,6 +6,11 @@ import AmplifySection from "./AmplifySection";
 import GameEvents from "./GameEvents";
 
 const Game = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    document.title = 'Game'
+  }, []);
+  
   return (
     <div className="container-fluid token-wrapper px-0">
       <div className="d-flex flex-column gap-5">
--- src/screens/Game/GameHero.js
+++ src/screens/Game/GameHero.js
@@ -2,12 +2,15 @@ import React, { useRef, useState } from "react";
 import epicblack from "../../assets/epicblack.svg";
 import useWindowSize from "../../hooks/useWindowSize";
 import Slider from "react-slick";
-import { NavLink } from "react-router-dom";
+import MapPopup from "../../components/PackagePopups/MapPopup";
 import BetaEventCardHome from "../Marketplace/components/BetaEventCardHome";
+import LandPopup from "../../components/PackagePopups/LandPopup";
+import CawsPopup from "../../components/PackagePopups/CawsPopup";
 
 const GameHero = () => {
   const [activeSlide, setActiveSlide] = useState();
   const [showFirstNext, setShowFirstNext] = useState();
+  const [showPopup, setshowPopup] = useState("");
 
   var settings = {
     dots: false,
@@ -70,19 +73,19 @@ const GameHero = () => {
   const windowSize = useWindowSize();
   const dummyBetaPassData2 = [
     {
-      link: "/token",
+      state: "map",
       title: "MAPS",
       desc: "Unique game world environments",
       class: "tokenClass",
     },
     {
-      link: "/earn",
+      state: "land",
       title: "LAND",
       desc: "Own and develop virtual areas",
       class: "earnClass",
     },
     {
-      link: "/marketplace/events/treasure-hunt",
+      state: "caws",
       title: "CAWS",
       desc: "AI-driven digital companions",
       class: "eventClass",
@@ -90,51 +93,80 @@ const GameHero = () => {
   ];
 
   return (
-    <div className="about-wrapper video-wrapper position-relative d-flex align-items-center flex-column justify-content-center mt-5 mt-lg-0 gap-5 position-relative">
-      <div className="custom-container  mt-5 mt-lg-0">
-        <div className="d-flex w-100 flex-column align-items-center gap-2">
-          <h2 className="font-montserrat main-hero-title  px-0">
-            What is <br/>World of Dypians?
-          </h2>
-          <p className="about-hero-desc">
-            An action-packed multiplayer game where heroes come together to
-            defend their planet against the Dark Lord's invasion. Choose from a
-            variety of heroes, each with unique abilities, and join forces with
-            other players to destroy the enemy's bases.
-          </p>
-          <div className="d-flex align-items-center gap-3">
-            <a
-              className="stake-wod-btn px-4 py-1"
-              href="https://store.epicgames.com/p/world-of-dypians-2e0694"
-              target="_blank"
-            >
-              <img
-                src={epicblack}
-                alt="icon"
-                className="epicgame2"
-                style={{ width: "auto", height: "30px" }}
-              />{" "}
-              Download
-            </a>
+    <>
+      <div className="about-wrapper video-wrapper position-relative d-flex align-items-center flex-column justify-content-center mt-5 mt-lg-0 gap-5 position-relative">
+        <div className="custom-container  mt-5 mt-lg-0">
+          <div className="d-flex w-100 flex-column align-items-center gap-2">
+            <h2 className="font-montserrat main-hero-title  px-0">
+              What is <br />
+              World of Dypians?
+            </h2>
+            <p className="about-hero-desc">
+              An action-packed multiplayer game where heroes come together to
+              defend their planet against the Dark Lord's invasion. Choose from
+              a variety of heroes, each with unique abilities, and join forces
+              with other players to destroy the enemy's bases.
+            </p>
+            <div className="d-flex align-items-center gap-3">
+              <a
+                className="stake-wod-btn px-4 py-1"
+                href="https://store.epicgames.com/p/world-of-dypians-2e0694"
+                target="_blank"
+              >
+                <img
+                  src={epicblack}
+                  alt="icon"
+                  className="epicgame2"
+                  style={{ width: "auto", height: "30px" }}
+                />{" "}
+                Download
+              </a>
+            </div>
           </div>
-        </div>
-        {windowSize.width > 992 && (
-          <div className="opacitywrapper custom-container">
+          {/* {windowSize.width > 992 && ( */}
+          <div className="opacitywrapper custom-container custom-wrapper">
             <Slider {...settings} ref={betaSlider}>
-              {dummyBetaPassData2.slice(0, 4).map((item, index) => (
-                <NavLink to={`${item.link}`}>
+              {dummyBetaPassData2.map((item, index) => (
+                <div
+                  onClick={() => {
+                    setshowPopup(item.state);
+                  }}
+                >
                   <BetaEventCardHome
                     data={item}
                     key={index}
                     isFrontPage={true}
                   />
-                </NavLink>
+                </div>
               ))}
             </Slider>
           </div>
-        )}
+          {/* )} */}
+        </div>
       </div>
-    </div>
+      {showPopup === "map" && (
+        <MapPopup
+          onClosePopup={() => {
+            setshowPopup("");
+          }}
+        />
+      )}
+      {showPopup === "land" && (
+        <LandPopup
+          onClosePopup={() => {
+            setshowPopup("");
+          }}
+        />
+      )}
+
+      {showPopup === "caws" && (
+        <CawsPopup
+          onClosePopup={() => {
+            setshowPopup("");
+          }}
+        />
+      )}
+    </>
   );
 };
 
