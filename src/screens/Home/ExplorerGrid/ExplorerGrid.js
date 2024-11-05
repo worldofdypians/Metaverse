468513e2f3a5fa92f0c147ec98fff72aa80fc376
LorenaL57 <lorena.lici579@gmail.com>
2024-11-05 11:19:52 +0100

    updated stats

--- src/screens/Home/ExplorerGrid/ExplorerGrid.js
+++ src/screens/Home/ExplorerGrid/ExplorerGrid.js
@@ -1,38 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./_explorergrid.scss";
-import playIcon from "../../../assets/playIcon.svg";
+import getFormattedNumber from "../../Caws/functions/get-formatted-number";
+// import playIcon from "../../../assets/playIcon.svg";
 
-const ExplorerGrid = () => {
+const ExplorerGrid = ({ totalSupply }) => {
+  const [totalTx2, setTotalTx] = useState(0);
+  const [totalvolume2, setTotalVolume] = useState(0);
+  const fetchCachedData = () => {
+    const cachedVolume = localStorage.getItem("cachedVolume");
+    const cachedTvl = localStorage.getItem("cachedTvl");
 
+    if (cachedTvl && cachedVolume) {
+      setTotalTx(cachedTvl);
+      setTotalVolume(cachedVolume);
+    }
+  };
+
+  useEffect(() => {
+    fetchCachedData();
+  }, []);
 
   return (
-    <div className="px-3 px-lg-5 py-4 stats-wrapper d-flex justify-content-center" id="explorer">
+    <div
+      className="px-3 px-lg-5 py-4 stats-wrapper d-flex justify-content-center"
+      id="explorer"
+    >
       <div className="custom-container ">
-      <div className="row">
-      <div className="col-12 col-lg-3 d-flex align-items-center justify-content-start">
-        <h2 className="font-montserrat builders-title explorer-grid-title px-0">
-          IMPORTANT STATS
-        </h2>
-      </div>
-      <div className="col-12 col-lg-3 mb-3 mb-lg-0">
-        <div className="new-stats-wrapper px-4 py-5 d-flex flex-column align-items-center justify-content-center gap-2">
-          <h6 className="mb-0 new-stats-value">38,841,100+</h6>
-          <span className="new-stats-type">Total on-chain transactions</span>
+        <div className="row">
+          <div className="col-12 col-lg-3 d-flex align-items-center justify-content-start">
+            <h2 className="font-montserrat builders-title explorer-grid-title px-0">
+              IMPORTANT STATS
+            </h2>
+          </div>
+          <div className="col-12 col-lg-3 mb-3 mb-lg-0">
+            <div className="new-stats-wrapper px-4 py-5 d-flex flex-column align-items-center justify-content-center gap-2">
+              <h6 className="mb-0 new-stats-value">
+                {" "}
+                {getFormattedNumber(totalTx2).slice(
+                  0,
+                  getFormattedNumber(totalTx2).length - 3
+                )}
+              </h6>
+              <span className="new-stats-type">
+                Total on-chain transactions
+              </span>
+            </div>
+          </div>
+          <div className="col-12 col-lg-3 mb-3 mb-lg-0">
+            <div className="new-stats-wrapper px-4 py-5 d-flex flex-column align-items-center justify-content-center gap-2">
+              <h6 className="mb-0 new-stats-value">
+                ${getFormattedNumber(totalvolume2, 0)}
+              </h6>
+              <span className="new-stats-type">Total Volume (USD)</span>
+            </div>
+          </div>
+          <div className="col-12 col-lg-3 mb-3 mb-lg-0">
+            <div className="new-stats-wrapper px-4 py-5 d-flex flex-column align-items-center justify-content-center gap-2">
+              <h6 className="mb-0 new-stats-value">
+                {getFormattedNumber(totalSupply, 0)}
+              </h6>
+              <span className="new-stats-type">Sold NFTs</span>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="col-12 col-lg-3 mb-3 mb-lg-0">
-         <div className="new-stats-wrapper px-4 py-5 d-flex flex-column align-items-center justify-content-center gap-2">
-          <h6 className="mb-0 new-stats-value">$9,693,600+</h6>
-          <span className="new-stats-type">Total Volume (USD)</span>
-        </div>
-      </div>
-      <div className="col-12 col-lg-3 mb-3 mb-lg-0">
-         <div className="new-stats-wrapper px-4 py-5 d-flex flex-column align-items-center justify-content-center gap-2">
-          <h6 className="mb-0 new-stats-value">99,280+</h6>
-          <span className="new-stats-type">Total on-chain transactions</span>
-        </div>
-      </div>
-    </div>
       </div>
     </div>
   );
