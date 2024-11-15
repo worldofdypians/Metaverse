8babd99054569d7dedd194e98ec851f4b0029c1a
AldiAlinj <aldialinj0@gmail.com>
2024-11-15 16:45:51 +0100

    working on game page

---
+++ src/screens/Game/FeatureSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './_game.scss'
+import gameLeaderboard from '../../assets/gameAssets/gameLeaderboard.svg'
+import gameGoldenPass from '../../assets/gameAssets/gameGoldenPass.png'
+
+const FeatureSection = () => {
+  return (
+    <div>FeatureSection</div>
+  )
+}
+
+export default FeatureSection
