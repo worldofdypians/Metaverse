2dc8336f086680053452682fd2752632e92ef54c
AldiAlinj <aldialinj0@gmail.com>
2024-11-16 10:31:21 +0100

    started working on new challenges section

---
+++ src/screens/Game/NewChallenges.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './_game.scss'
+
+const NewChallenges = () => {
+  return (
+   <div className="d-flex flex-column">
+    <div className="new-challenges-hero w-100 p-5"></div>
+   </div>
+  )
+}
+
+export default NewChallenges
