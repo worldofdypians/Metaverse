c8a1baaa6196a2f8b851003164d3dc75b0bc412b
LorenaL579 <lorena.lici579@gmail.com>
2025-01-23 08:36:06 +0100

    updated time

--- src/screens/TermsConditions/BinanceCampaignRules.js
+++ src/screens/TermsConditions/BinanceCampaignRules.js
@@ -30,8 +30,8 @@ const BinanceCampaignRules = () => {
                 <b>Activity Period:</b>
               </p>
               <ul>
-                <li className="text-white">Start: 2025-01-23 11:00 (UTC)</li>
-                <li className="text-white">End: 2025-02-06 11:00 (UTC)</li>
+                <li className="text-white">Start: 2025-01-23 12:00 (UTC)</li>
+                <li className="text-white">End: 2025-02-06 12:00 (UTC)</li>
               </ul>
               <p className="text-white m-0">
                 <b>Mission Details and Prizes:</b>
