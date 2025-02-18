f39238a2412e36932cf0fb971f180f14993980cd
AldiAlinj <aldialinj0@gmail.com>
2025-02-18 16:34:05 +0100

    fixed loader ui

--- src/screens/NewAgent/Agent.js
+++ src/screens/NewAgent/Agent.js
@@ -29,6 +29,8 @@ const Agent = ({ email, coinbase, handleConnectWallet, isConnected }) => {
     if (progress === 100 && !active) {
       setTimeout(() => setIsLoaded(true), 500); // Short delay to ensure stability
     }
+    console.log(active, "progress");
+    
   }, [progress, active]);
 
   useEffect(() => {
@@ -42,9 +44,12 @@ const Agent = ({ email, coinbase, handleConnectWallet, isConnected }) => {
 
   const fetchTries = async () => {
     try {
-      const res = await axios.get("https://api.worldofdypians.com/user-tries/", {
-        params: { userId: coinbase },
-      });
+      const res = await axios.get(
+        "https://api.worldofdypians.com/user-tries/",
+        {
+          params: { userId: coinbase },
+        }
+      );
       setTries(res.data.tries);
     } catch (error) {
       console.error("Error fetching tries:", error);
@@ -60,30 +65,67 @@ const Agent = ({ email, coinbase, handleConnectWallet, isConnected }) => {
       <div className="container-fluid d-flex bridge-mainhero-wrapper token-wrapper justify-content-center">
         <div className="d-flex flex-column w-100">
           <AgentHero openPopup={() => setPopup(true)} />
-          <div className="container-fluid d-flex justify-content-center" style={{ position: "relative", bottom: "30px" }}>
+          <div
+            className="container-fluid d-flex justify-content-center"
+            style={{ position: "relative", bottom: "30px" }}
+          >
             <div className="custom-container">
               <div className="row">
                 {windowSize.width > 786 && (
                   <div className={`col-12 col-lg-4 ${!toggle && "d-none"}`}>
-                    <div className="canvas-wrapper position-relative" style={{border: !isLoaded && "0.5px solid #080b2a"}}>
+                    <div
+                      className="canvas-wrapper position-relative"
+                      style={{ border: !isLoaded && "0.5px solid #080b2a" }}
+                    >
                       {toggle && (
-                        <div className="hide-oryn-btn d-flex align-items-center gap-2 p-2" onClick={() => setToggle(!toggle)}>
+                        <div
+                          className={`hide-oryn-btn d-flex align-items-center gap-2 p-2 ${!isLoaded && "d-none"} `}
+                          onClick={() => setToggle(!toggle)}
+                        >
                           <img src={eyeClosed} alt="eye-closed" /> Hide Oryn
                         </div>
                       )}
                       {sound ? (
-                        <img src={soundOn} width={40} height={40} alt="" onClick={() => setSound(!sound)} style={{ cursor: "pointer" }} className="sound-button-position" />
+                        <img
+                          src={soundOn}
+                          width={40}
+                          height={40}
+                          alt=""
+                          onClick={() => setSound(!sound)}
+                          style={{ cursor: "pointer" }}
+                          className={`sound-button-position ${!isLoaded && "d-none"}`}
+                        />
                       ) : (
-                        <img src={soundOff} width={40} height={40} alt="" onClick={() => setSound(!sound)} style={{ cursor: "pointer" }} className="sound-button-position" />
+                        <img
+                          src={soundOff}
+                          width={40}
+                          height={40}
+                          alt=""
+                          onClick={() => setSound(!sound)}
+                          style={{ cursor: "pointer" }}
+                          className={`sound-button-position ${!isLoaded && "d-none"}`}
+                        />
                       )}
                       {!isLoaded ? (
-                        <div className="custom-loader d-flex flex-column align-items-center justify-content-center gap-3">
-                          <span>{Math.round(progress)}%</span>
-                          <div className="loader-bar"></div>
+                        <div className="teleport-container">
+                          <div className="line"></div>
+                          <div className="line"></div>
+                          <div className="line"></div>
                         </div>
                       ) : (
-                        <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }} style={{ height: "60vh", pointerEvents: "none" }}>
-                          <Experience playAudio={playAudio} setPlayAudio={setPlayAudio} count={count} audioFile={audioFile} jsonFile={jsonFile} sound={sound} />
+                        <Canvas
+                          shadows
+                          camera={{ position: [0, 0, 8], fov: 42 }}
+                          style={{ height: "60vh", pointerEvents: "none" }}
+                        >
+                          <Experience
+                            playAudio={playAudio}
+                            setPlayAudio={setPlayAudio}
+                            count={count}
+                            audioFile={audioFile}
+                            jsonFile={jsonFile}
+                            sound={sound}
+                          />
                         </Canvas>
                       )}
                     </div>
@@ -116,7 +158,10 @@ const Agent = ({ email, coinbase, handleConnectWallet, isConnected }) => {
       </div>
       {popup && (
         <OutsideClickHandler onOutsideClick={() => setPopup(false)}>
-          <OrynPopup onClose={() => setPopup(false)} isConnected={isConnected} />
+          <OrynPopup
+            onClose={() => setPopup(false)}
+            isConnected={isConnected}
+          />
         </OutsideClickHandler>
       )}
     </>
