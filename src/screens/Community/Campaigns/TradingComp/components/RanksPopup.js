212f9e8eeec6244ac67d43b471c58bda0b174a2b
Aldi Alinj <aldialinj0@gmail.com>
2025-05-01 10:24:57 +0200

    fixed trading comp leaderboard

--- src/screens/Community/Campaigns/TradingComp/components/RanksPopup.js
+++ src/screens/Community/Campaigns/TradingComp/components/RanksPopup.js
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { shortAddress } from "../../../../Caws/functions/shortAddress";
 import getFormattedNumber from "../../../../Caws/functions/get-formatted-number";
 
 const RanksPopup = ({ participants, onClose, coinbase }) => {
+  const scrollRef = useRef(null);
+
+  const [slice, setSlice] = useState(300);
+
+  const onScrollEnd = () => {
+    setSlice(slice + 100);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const el = scrollRef.current;
+      if (el) {
+        const isBottom = el.scrollHeight - el.scrollTop === el.clientHeight;
+        if (isBottom) {
+          onScrollEnd && onScrollEnd(); // Dispatch your function
+        }
+      }
+    };
+
+    const el = scrollRef.current;
+    if (el) {
+      el.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (el) {
+        el.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, [onScrollEnd]);
+
   return (
     <div
       className="trading-popup-wrapper popup-active p-3"
       style={{ background: "#18193C", border: "none" }}
     >
-      <div class="overlay-shadow-2"></div>
+      <div className="overlay-shadow-2"></div>
       <div className="d-flex align-items-center justify-content-between w-100">
         <div className="d-flex flex-column flex-lg-row align-items-start align-items-lg-end gap-2">
           <h6 className="trading-popup-title mb-0">Weekly Rankings</h6>
-          {/* <span className="comp-lb-reset-time">Reset Time: <b>Monday 00:00 UTC</b></span> */}
         </div>
         <img
           src={"https://cdn.worldofdypians.com/wod/popupXmark.svg"}
@@ -21,6 +51,7 @@ const RanksPopup = ({ participants, onClose, coinbase }) => {
           alt=""
         />
       </div>
+
       <div className="row mt-3">
         <div className="col-2 col-lg-1 d-flex justify-content-start">
           <span className="comp-lb-header">Rank</span>
@@ -32,11 +63,16 @@ const RanksPopup = ({ participants, onClose, coinbase }) => {
           <span className="comp-lb-header">Volume(USD)</span>
         </div>
       </div>
-      <div className="d-flex trading-comp-overflow-3 flex-column gap-2 mt-3">
-        {participants.slice(0, 500).map((item, index) => (
+
+      <div
+        className="d-flex trading-comp-overflow-3 flex-column gap-2 mt-3"
+        ref={scrollRef}
+      >
+        {participants.slice(0, slice).map((item, index) => (
           <div
+            key={item.Address}
             className={`d-flex align-items-center ${
-              index + 1 === 30 && "pb-2"
+              index + 1 === 30 ? "pb-2" : ""
             } gap-4`}
             style={{
               borderBottom: index + 1 === 30 ? "1px solid red" : "none",
@@ -44,16 +80,16 @@ const RanksPopup = ({ participants, onClose, coinbase }) => {
           >
             <div
               className={`trading-comp-lb-rank ${
-                index + 1 <= 30 && "lb-winner"
+                index + 1 <= 30 ? "lb-winner" : ""
               } d-flex align-items-center justify-content-center`}
             >
               {index + 1}
             </div>
             <div
               className={`trading-comp-lb-item-2 ${
-                coinbase === item.Address && "trading-comp-lb-item-player"
+                coinbase === item.Address ? "trading-comp-lb-item-player" : ""
               } ${
-                index + 1 <= 30 && "lb-winner"
+                index + 1 <= 30 ? "lb-winner" : ""
               } p-2 d-flex w-100 align-items-center justify-content-between`}
             >
               <div className="d-flex align-items-center gap-2">
