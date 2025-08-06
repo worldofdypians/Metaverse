909770d9f90c96e5b855a350338b5d1f73288f2a
AldiAlinj <aldialinj0@gmail.com>
2025-08-06 09:53:24 +0200

    added gif to kickstarter page

--- src/components/Kickstarter/components/KickstarterApply.js
+++ src/components/Kickstarter/components/KickstarterApply.js
@@ -7,7 +7,7 @@ const KickstarterApply = () => {
             <div className="d-flex flex-column gap-2 align-items-center">
                          <h6 className='main-hero-title font-montserrat text-start'>Ready to Kickstart Your Growth?</h6>
                         <p className="market-banner-desc font-montserrat text-center w-75">Take your project from launch to mainstream adoption. Plug into a fully gamified, AI-powered metaverse that rewards, educates, and activates users on-chain.</p>
-                        <button className="explore-btn px-3 py-2">Start Your Kickstart Journey Now</button>
+                        <a href='https://forms.gle/MUYyzKpS23yrzGcB7' target='_blank' rel='noreferrer' className="explore-btn px-3 py-2">Start Your Kickstart Journey Now</a>
             </div>
         </div>
     </div>
--- src/components/Kickstarter/components/KickstarterHero.js
+++ src/components/Kickstarter/components/KickstarterHero.js
@@ -7,10 +7,10 @@ const KickstarterHero = () => {
         <div className="custom-container">
             <div className="row">
                 <div className="col-12 col-lg-6">
-                    <div className="d-flex flex-column gap-2">
+                    <div className="d-flex flex-column gap-2 align-items-center align-items-lg-start">
                         <h6 className='main-hero-title font-montserrat text-start'>Kickstart Program</h6>
                         <p className="market-banner-desc font-montserrat">Join the World of Dypians Kickstarter, a gateway for bold creators, builders, and partners to launch exclusive experiences, boost brand visibility, and drive real engagement growth within the game.</p>
-                        <button className="explore-btn px-3 py-2">Apply Now</button>
+                        <a href='https://forms.gle/MUYyzKpS23yrzGcB7' target='_blank' rel='noreferrer' className="explore-btn px-3 py-2">Apply Now</a>
                     </div>
                 </div>
             </div>
