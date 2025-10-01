628f76774af7eabc48d1c8243c679e7d13eb440e
LorenaL579 <lorena.lici579@gmail.com>
2025-10-01 11:04:38 +0200

    updated profile

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -1245,9 +1245,9 @@ const MyProfile = ({
                     />
                     <div className="daily-progress-value-golden">
                       <span>
-                        {userActiveEvents === 6
+                        {userActiveEvents === 7
                           ? "Completed"
-                          : userActiveEvents + "/6"}
+                          : userActiveEvents + "/7"}
                       </span>
                     </div>
 
