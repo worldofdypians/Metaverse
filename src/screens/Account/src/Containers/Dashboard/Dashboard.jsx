7ef1542a9eea55bef72423f56454dec318c2d7a7
LorenaL579 <lorena.lici579@gmail.com>
2026-06-13 23:24:54 +0200

    updates for vanar leaderboard

--- src/screens/Account/src/Containers/Dashboard/Dashboard.jsx
+++ src/screens/Account/src/Containers/Dashboard/Dashboard.jsx
@@ -4767,10 +4767,10 @@ function Dashboard({
         type: "stars",
         rewards: vanarStars,
         previous_rewards: vanarStars,
-        activeData: dailyRecordsVanar,
-        previousData: prevDataVanar,
-        player_data: userDataVanar,
-        is_active: activePlayerVanar,
+        activeData: [],
+        previousData: [],
+        player_data: {},
+        is_active: true,
         loading: loadingVanar,
       },
     ]);
@@ -5016,7 +5016,7 @@ function Dashboard({
       // }
     } else if (chain === "vanar") {
       // if (dailyRecordsVanar.length === 0) {
-      fetchDailyRecordsVanar();
+      // fetchDailyRecordsVanar();
       // }
     }
     // else if (chain === "matchain") {
