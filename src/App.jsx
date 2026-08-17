6d9f80c42d6deafd63d5c1bb16419b03405545f1
LorenaL57 <lorena.lici579@gmail.com>
2026-08-17 14:47:54 +0200

    updated bnb treasrue hunt

--- src/App.jsx
+++ src/App.jsx
@@ -811,7 +811,7 @@ function AppRoutes() {
   let cmcLastDay = new Date("2026-07-09T14:00:00.000+02:00");
   let dypius2LastDay = new Date("2024-05-27T16:00:00.000+02:00");
   let teaLastDay = new Date("2025-10-16T13:00:00.000+02:00");
-  let bnbLastDay = new Date("2026-08-26T14:00:00.000+02:00");
+  let bnbLastDay = new Date("2026-09-15T14:00:00.000+02:00");
   let coreLastDay = new Date("2025-04-04T14:00:00.000+02:00");
   let victionLastDay = new Date("2025-03-29T14:00:00.000+02:00");
   let coreLastDay2 = new Date("2026-05-19T14:00:00.000+02:00");
